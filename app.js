8fd473b1461619a7beb912291dad39722119d63a
prabhjot singh <48572962+prabh234@users.noreply.github.com>
2023-09-13 16:47:19 +0530

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bp = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static("public"));
@@ -16,10 +16,6 @@ const userSchema =new mongoose.Schema({
     email:String,
     password:String
 });
-
-let secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password']  });
 
 
 const User = new mongoose.model('User',userSchema)
@@ -37,7 +33,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
     let newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save().then(r=>{
         console.log("registered successfully");
@@ -49,9 +45,8 @@ app.post("/register",(req,res)=>{
 app.post("/login",(req,res)=>{
     let eml = req.body.username;
     User.findOne({email:eml}).then(user=>{
-        console.log(user.password);
         if(user){
-            if (user.password === req.body.password) {
+            if (user.password === md5(req.body.password)) {
                 res.render("secrets")
             }
             else{
